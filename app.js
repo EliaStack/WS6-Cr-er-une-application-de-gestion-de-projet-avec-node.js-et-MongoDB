8b4394a264939e79eafd9eeb18e16633332a9d4a
Elia <elia.soinard@outlook.fr>
2026-05-13 14:42:03 +0200

    Part 1-5

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express'); //Pour importer express
 
 const app = express(); //Pour créer une app
 
-app.use((req,res)=>{ //Emission/réception du serveur
-    res.json({message: 'Votre requête a bien été reçue'});
+app.use((req, res, next) => {
+    console.log('Requête reçue');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => { //Emission/réception du serveur
+    res.json({ message: 'Votre requête a bien été reçue' });
+    next();
 })
 
+app.use((req,res)=>{ //Pas de 'next()' parce que dernier middlewear
+console.log('Réponse envoyée avec suucès');
+});
 
 module.exports = app; //Exporte les fichiers pour les rendres accesibles nottament avec serveur node
 

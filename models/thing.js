aad5b3b1d50860a198301c1a6cb896db1c5025c3
Elia <elia.soinard@outlook.fr>
2026-05-14 10:33:29 +0200

    Add folder models with thing and export

---
+++ models/thing.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({ //Par rapport à app.js pas besoin add id car Mongo fait en auto
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    userId: { type: String, required: true },
+    price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('thing', thingSchema); //Exporter le modèle avec les données
+//thing = nom du model, thingSchema=nom de la const
+
+
+ 

27b49aed36ec9a5b63ecc14b0cc75ff9624273ed
Elia <elia.soinard@outlook.fr>
2026-05-14 18:49:35 +0200

    Configuration de Multer

---
+++ middleware/multer-config.js
@@ -0,0 +1,25 @@
+const multer = require('multer');
+const { useCallback } = require('react');
+
+console.log('Multer est en route');
+
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+};
+
+
+//Objet de configuration pour multer
+const storage = multer.diskStorage({ //Enregister sur le disque
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_'); //Sa élimine le pb des espaces
+        const extension = MIME_TYPES[file.mimetype]; //Correspond au mimetype envoyé par le frontend
+        callback(null, name + Date.now() + '.' + extension); //Création du nom de fichier
+    }
+});
+
+module.exports = multer({ storage: storage }).single('image'); //single=fichier unique, image=ne transporte que les images

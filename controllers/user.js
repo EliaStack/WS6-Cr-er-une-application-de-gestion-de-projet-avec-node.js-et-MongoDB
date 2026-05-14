b387ed487fbd0ae815c27bb2fc6b7b45bcdf4652
Elia <elia.soinard@outlook.fr>
2026-05-14 17:11:27 +0200

    Suite Auth

--- controllers/user.js
+++ controllers/user.js
@@ -6,18 +6,41 @@ exports.signup = (req, res, next) => { //Enregistrement de new user
         .then(hash => {
             const user = new user({ //Nouvelle user
                 email: req.body.email,
-                password:hash
+                password: hash
             });
             user.save() //Enregistre ds la base de données
-            .then(()=> res.status(201).json({message:'Utilisateur créer !'}))
-            .catch(error => res.status(400).json({ error }));
+                .then(() => res.status(201).json({ message: 'Utilisateur créer !' }))
+                .catch(error => res.status(400).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
 };
 
-
-
 exports.login = (req, res, next) => { //Connecter des utilisateurs existants
+    user.findOne({ email: req.body.email })
+        .then(user => {
+            if (user === null) {
+                res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
+                        } else {
+                            res.status(200).json({
+                                //Nécessaire pour l'auth des requête émise par le client
+                                userId: user._id,
+                                token: 'TOKEN'
+                            });
+                        }
+                    })
+                    .catche(error => {
+                        res.status(500).json({ error }); //Erreur serveur
+                    })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error }); //Erreur serveur
+        })
 
 };
 
